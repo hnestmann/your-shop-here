8e38a718909caa9dce2942b075a56b03fe293868
Danny Gehl <dgehl@salesforce.com>
2023-11-03 19:19:27 +0100

    Added key controllers

---
+++ commerce-cloud-code/app_yourshophere/cartridge/controllers/Error.js
@@ -0,0 +1,46 @@
+'use strict';
+
+var server = require('server');
+var system = require('dw/system/System');
+var Resource = require('dw/web/Resource');
+
+server.use('Start', function (req, res, next) {
+    res.setStatusCode(500);
+    var showError = system.getInstanceType() !== system.PRODUCTION_SYSTEM
+        && system.getInstanceType() !== system.STAGING_SYSTEM;
+    if (req.httpHeaders.get('x-requested-with') === 'XMLHttpRequest') {
+        res.json({
+            error: req.error || {},
+            message: Resource.msg('subheading.error.general', 'error', null)
+        });
+    } else {
+        res.render('error/error', {
+            error: req.error || {},
+            showError: showError,
+            message: Resource.msg('subheading.error.general', 'error', null)
+        });
+    }
+    next();
+});
+
+server.use('ErrorCode', function (req, res, next) {
+    res.setStatusCode(500);
+    var errorMessage = 'message.error.' + req.querystring.err;
+
+    res.render('error/error', {
+        error: req.error || {},
+        message: Resource.msg(errorMessage, 'error', null)
+    });
+    next();
+});
+
+server.get('Forbidden', function (req, res, next) {
+    var URLUtils = require('dw/web/URLUtils');
+    var CustomerMgr = require('dw/customer/CustomerMgr');
+
+    CustomerMgr.logoutCustomer(true);
+    res.redirect(URLUtils.url('Home-Show'));
+    next();
+});
+
+module.exports = server.exports();
