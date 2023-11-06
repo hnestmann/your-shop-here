d3ac5cd5379d986650745f24f5cb61c685533cd7
Holger Nestmann <hnestmann@salesforce.com>
2023-11-06 20:09:57 +0100

    image responsiveness

--- commerce-cloud-code/app_yourshophere/cartridge/experience/utilities/imageSourceSet.js
+++ commerce-cloud-code/app_yourshophere/cartridge/experience/utilities/imageSourceSet.js
@@ -1,8 +1,10 @@
 var ImageTransformation = {};
+var URLUtils = require('dw/web/URLUtils');
 const imageSizes = require('*/cartridge/experience/imageSizes.json');
 
 module.exports = function createSourceSet(image) {
-    const srcset = imageSizes.sizes.map(width => `${image.file.getImageURL({scaleWidth: width})} ${width}px`).join(',');
+    const srcset = imageSizes.sizes.map(width => `${image.file.getImageURL({scaleWidth: width, ${width}w`).join(', \n');
     const defaultsize = image.file.getImageURL({scaleWidth: imageSizes.default});
-    return {srcset: srcset, defaultsize: defaultsize};
+    const aspectRatio = `${image.metaData.getWidth()} / ${image.metaData.getHeight()}`
+    return {srcset: srcset, defaultsize: defaultsize, aspectRatio: aspectRatio};
 };
