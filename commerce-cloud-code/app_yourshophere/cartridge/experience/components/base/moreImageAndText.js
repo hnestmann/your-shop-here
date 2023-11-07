7512c3a3f3c71248a55f4ee4b103ebad6b1b4e23
Holger Nestmann <hnestmann@salesforce.com>
2023-11-07 08:07:55 +0100

    add placeholder image

--- commerce-cloud-code/app_yourshophere/cartridge/experience/components/base/moreImageAndText.js
+++ commerce-cloud-code/app_yourshophere/cartridge/experience/components/base/moreImageAndText.js
@@ -4,7 +4,7 @@
 
 const Template = require('dw/util/Template');
 const HashMap = require('dw/util/HashMap');
-const imageSourceSet = require('*/cartridge/experience/utilities/imageSourceSet.js');
+const imageContainerDecorator = require('*/cartridge/experience/utilities/decorator/imageContainer.js');
 
 /**
  * Render logic for storefront.imageAndText component.
@@ -13,52 +13,12 @@ const imageSourceSet = require('*/cartridge/experience/utilities/imageSourceSet.
  */
 module.exports.render = function (context) {
     const content = context.content;
-    
-    const overlayTextClasses = [];
-    const model = new HashMap();
-    const colorMap = {
-        Primary: '-primary',
-        'Page Background': '-bgcolor',
-        'Link Color': '-interation ',
-        'Menu Background': '-menucolor'
-    };
-    model.heading = content.heading ? content.heading : '';
-    model.ITCText = content.ITCText ? content.ITCText : '';
-    model.link = content.link;
-    model.ITCText = content.ITCText ? content.ITCText : '';
-    model.link = content.ITCLink ? content.ITCLink : '#';
-    model.alt = content.alt ? content.alt : '';
-    model.brightness = content.Brightness ? content.Brightness : 100;
-    if (content.textContrast === 'Drop Shadow') {
-        overlayTextClasses.push('drop-shadow-contrast');
-    }
-    if (content.textContrast === 'Background Color') {
-        overlayTextClasses.push('bg' + colorMap[content.textBackground]);
-    }
-    if (content.textColor && colorMap[content.textColor]) {
-        overlayTextClasses.push(' fg' + colorMap[content.textColor]);
-    }
-    if (content.textHAlign) {
-        overlayTextClasses.push('text-' + content.textHAlign);
-        model.containerHAlign = 'halign-' + content.textHAlign;
-    } else {
-        overlayTextClasses.push('text-center');
-        model.containerHAlign = 'halign-center';
-    }
-
-    if (content.textVAlign) {
-        model.verticalAlign = 'valign-' + content.textVAlign;
-    } else {
-        model.verticalAlign = 'valign-middle';
-    }
-
-    model.overlayTextClass = overlayTextClasses.join(' ');
-    model.mainClass = (content.textContrast === 'Gradient') ? 'gradient-contrast' : '';
-
-    model.image = imageSourceSet(content.image);
+    const tmpModel = Object.create(null);
+    imageContainerDecorator(tmpModel, content);
+    const model = tmpModel.imageContainer;
     model.heading = model.heading.replace('<p>','');
     model.heading = model.heading.replace(new RegExp('</p>$'),'');
-
+    const hxdebug = model.link.includes('?');
     model.hxlink = `${model.link}${(model.link.includes('?') ? '&' : '?')}hx=main`;
     return new Template('experience/components/base/moreImageAndText').render(model).text;
 };
--- commerce-cloud-code/app_yourshophere/cartridge/experience/utilities/decorator/imageContainer.js
+++ commerce-cloud-code/app_yourshophere/cartridge/experience/utilities/decorator/imageContainer.js
@@ -1,10 +1,55 @@
-var HashMap = require('dw/util/HashMap');
+const HashMap = require('dw/util/HashMap');
+const imageSourceSet = require('*/cartridge/experience/utilities/imageSourceSet.js');
 
 module.exports = function (object, content) {
     Object.defineProperty(object, 'imageContainer', {
         enumerable: true,
         value: (function () {
+            const overlayTextClasses = [];
+            const model = new HashMap();
+            const ImageTransformation = require('*/cartridge/experience/utilities/ImageTransformation.js');
+            const colorMap = {
+                Primary: '-primary',
+                'Page Background': '-bgcolor',
+                'Link Color': '-interation ',
+                'Menu Background': '-menucolor'
+            };
+            model.heading = content.heading ? content.heading : null;
+            model.ITCText = content.ITCText ? content.ITCText : null;
             
+            model.image = imageSourceSet(content.image);
+            
+            model.link = content.link;
+            model.ITCText = content.ITCText ? content.ITCText : null;
+            model.link = content.ITCLink ? content.ITCLink : '#';
+            model.alt = content.alt ? content.alt : null;
+            model.brightness = content.Brightness ? content.Brightness : 100;
+            if (content.textContrast === 'Drop Shadow') {
+                overlayTextClasses.push('drop-shadow-contrast');
+            }
+            if (content.textContrast === 'Background Color') {
+                overlayTextClasses.push('bg' + colorMap[content.textBackground]);
+            }
+            if (content.textColor && colorMap[content.textColor]) {
+                overlayTextClasses.push(' fg' + colorMap[content.textColor]);
+            }
+            if (content.textHAlign) {
+                overlayTextClasses.push('text-' + content.textHAlign);
+                model.containerHAlign = 'halign-' + content.textHAlign;
+            } else {
+                overlayTextClasses.push('text-center');
+                model.containerHAlign = 'halign-center';
+            }
+
+            if (content.textVAlign) {
+                model.verticalAlign = 'valign-' + content.textVAlign;
+            } else {
+                model.verticalAlign = 'valign-middle';
+            }
+
+            model.overlayTextClass = overlayTextClasses.join(' ');
+            model.mainClass = (content.textContrast === 'Gradient') ? 'gradient-contrast' : '';
+
             return model;
         }())
     });
