e86a5286fe4f0d08c655145a00abf433db19d293
Holger Nestmann <hnestmann@salesforce.com>
2023-11-07 08:07:55 +0100

    add placeholder image

--- commerce-cloud-code/app_yourshophere/cartridge/experience/utilities/imageSourceSet.js
+++ commerce-cloud-code/app_yourshophere/cartridge/experience/utilities/imageSourceSet.js
@@ -1,10 +1,10 @@
-var ImageTransformation = {};
-var URLUtils = require('dw/web/URLUtils');
+const URLUtils = require('dw/web/URLUtils');
 const imageSizes = require('*/cartridge/experience/imageSizes.json');
 
 module.exports = function createSourceSet(image) {
     const srcset = imageSizes.sizes.map(width => `${image.file.getImageURL({scaleWidth: width})} ${width}w`).join(', \n');
     const defaultsize = image.file.getImageURL({scaleWidth: imageSizes.default});
+    const mini = image.file.getImageURL({scaleWidth: imageSizes.miniplaceholder, format: 'png'});
     const aspectRatio = `${image.metaData.getWidth()} / ${image.metaData.getHeight()}`
-    return {srcset: srcset, defaultsize: defaultsize, aspectRatio: aspectRatio};
+    return {srcset: srcset, defaultsize: defaultsize, aspectRatio: aspectRatio, mini: mini};
 };
