33ae141bf8bce7f633243aea3370318c16c5382d
Holger Nestmann <hnestmann@salesforce.com>
2023-11-08 07:52:53 +0100

    update responsive images

--- commerce-cloud-code/app_yourshophere/cartridge/experience/components/base/moreImageAndText.js
+++ commerce-cloud-code/app_yourshophere/cartridge/experience/components/base/moreImageAndText.js
@@ -6,6 +6,7 @@ const Template = require('dw/util/Template');
 const HashMap = require('dw/util/HashMap');
 const imageContainerDecorator = require('*/cartridge/experience/utilities/decorator/imageContainer.js');
 
+
 /**
  * Render logic for storefront.imageAndText component.
  * @param {dw.experience.ComponentScriptContext} context The Component script context object.
@@ -20,5 +21,41 @@ module.exports.render = function (context) {
     model.heading = model.heading.replace(new RegExp('</p>$'),'');
     const hxdebug = model.link.includes('?');
     model.hxlink = `${model.link}${(model.link.includes('?') ? '&' : '?')}hx=main`;
-    return new Template('experience/components/base/moreImageAndText').render(model).text;
+    return print(model);
 };
+
+function print(model) {
+    return `
+        <figure class="image-component ${model.mainClass}">
+            <a href="${model.link}" aria-label="${model.alt}">
+                <img class="common-image-component"
+                    style="
+                        aspect-ratio: ${model.image.aspectRatio};
+                        background-image: url(${model.image.mini});
+                    "
+                    loading="lazy"
+                    srcset="${model.image.srcset}"
+                    sizes="${model.image.sizes}"
+                    src="${model.image.defaultsize}"
+                    alt="${model.alt}" title="${model.alt}" 
+                />
+            </a>
+            <figcaption>
+                ${!empty(model.heading) ? (`
+                    <span class="image-on ${model.overlayTextClass} ${model.verticalAlign} ${model.containerHAlign}">
+                        <a href="${model.link}" hx-push-url="${model.link}"
+                            aria-label="${model.alt}" hx-get="${model.hxlink}" hx-target="main" hx-indicator=".progress">
+                            ${model.heading}
+                        </a>
+                    </span>
+                `) : ''}
+                ${!empty(model.ITCText) ? (`
+                    <span class="image-below">
+                        ${model.ITCText}
+                    </span>
+                `) : ''}
+            
+            </figcaption>
+        </figure>
+    `
+}
