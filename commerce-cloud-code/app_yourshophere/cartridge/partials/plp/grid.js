48f8ad47b5b12d377c7e7518cf601fdafc5b11b4
Holger Nestmann <hnestmann@salesforce.com>
2024-01-05 18:14:13 +0100

    fix "paging"

--- commerce-cloud-code/app_yourshophere/cartridge/partials/plp/grid.js
+++ commerce-cloud-code/app_yourshophere/cartridge/partials/plp/grid.js
@@ -12,7 +12,32 @@ exports.createModel = () => {
     const search = require('api/ProductSearchModel');
     search.init(httpParams);
     search.search();
+<<<<<<< Updated upstream
     return search.foundProducts;
+=======
+
+    const componentId = httpParams.get('component');
+    model.products = search.foundProducts.map((hit) => ({ tileUrl: hit.tileUrl.append('component', componentId) }));
+    model.showMoreButton = (search.pagePosition + search.pageSize) < search.resultCount;
+    // @todo makes only sense with proper page controls
+    model.moreUrlFull = search.nextPageUrl('Search-Show').toString();
+    model.moreUrlHx = search.nextPageUrl('Search-Grid');
+    model.moreUrlHx = model.moreUrlHx.append('component', componentId);
+    return model;
+};
+
+function templateIncludeHit(hit) {
+    return `<wainclude url="${hit.tileUrl}"/>`;
+}
+
+function templateIncludeMore(model) {
+    return `<div id="search-more">
+        <a role="button" href="${model.moreUrlFull}"  hx-get="${model.moreUrlHx}" hx-target="#search-more" hx-swap="outerHTML">
+            <!-- // @todo localization -->
+            More
+        </a>
+    </div>`;
+>>>>>>> Stashed changes
 }
 
 exports.template = (model) => `
