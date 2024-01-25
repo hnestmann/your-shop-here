36ca4774f4f1caf154581163d7cbf92d9b8d71c6
Danny Gehl <dgehl@salesforce.com>
2024-01-25 13:46:53 +0100

    Better solution for minicart

--- commerce-cloud-code/app_yourshophere/cartridge/partials/cart/addtocart.js
+++ commerce-cloud-code/app_yourshophere/cartridge/partials/cart/addtocart.js
@@ -15,7 +15,6 @@ exports.createModel = function createModel(options) {
     return model;
 };
 
-// @todo get icon from skin PD page, instead hard coding here
 exports.template = model => `<dialog open>
 <article>
     <a href="#close"
@@ -34,9 +33,6 @@ exports.template = model => `<dialog open>
   </footer>
 </article>
 </dialog>
-<div hx-swap-oob="innerHTML:.experience-theme-minicart">
-  <a href="/on/demandware.store/Sites-YourShopHere-Site/default/Cart-Show" class="pill" role="button">
-  <svg style="width:35px;" viewBox="0 0 24 24" id="cart" xmlns="http://www.w3.org/2000/svg"><g><path d="M9.3 12h11c.3 0 .7-.2.7-.6l2-7.1c.2-.5-.2-.9-.7-.9h-17l-.3-1c-.1-.6-.6-.9-1-.9H2.1C1.5 1.5 1 2 .9 2.6c0 .6.5 1.2 1.1 1.2h1.1l3.5 11.8c.1.6.6.9 1.1.9h13c.6 0 1.1-.5 1.2-1.1 0-.6-.5-1.2-1.1-1.2H9.3c-.5 0-.9-.3-1-.8-.3-.7.3-1.4 1-1.4z"></path><circle cx="9.508" cy="20.585" r="1.846"></circle><circle cx="18.508" cy="20.585" r="1.846"></circle></g></svg>
+<div hx-swap-oob="innerHTML:#minicart-items">
     ${model.itemCount}
-  </a>
 </div>`;
