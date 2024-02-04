e25190c18fe7a9d1b657338c5f429ab4ca4e1978
Holger Nestmann <hnestmann@salesforce.com>
2024-02-04 21:05:59 +0100

    Update submitButton.js

--- commerce-cloud-code/app_yourshophere/cartridge/partials/checkout/submitButton.js
+++ commerce-cloud-code/app_yourshophere/cartridge/partials/checkout/submitButton.js
@@ -2,13 +2,16 @@
 exports.createModel = function createModel(input) {
     const BasketMgr = require('dw/order/BasketMgr');
     const basket = BasketMgr.getCurrentOrNewBasket();
+
     const URLUtils = require('dw/web/URLUtils');
     const model = {};
     // self-reference, so Checkout Save Addresses comes back here.
+    // If you base your own pd components on this, dont copy and paste this without swapping the partial
     session.privacy.submitPartialId = 'checkout/submitButton';
 
     model.actionUrl = URLUtils.url('Checkout-Submit');
     const hasShippingmethod = !!basket.defaultShipment.shippingMethod;
+    const hasPaymentMethod = !basket.paymentInstruments.empty;
     const hasBillingAddress = !!basket.billingAddress;
     const hasShippingAddress = !!basket.defaultShipment.shippingAddress;
     model.isReady = hasBillingAddress && hasShippingAddress && hasShippingmethod && hasPaymentMethod;
