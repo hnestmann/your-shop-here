ebcad292ceaf853c5f2293c51d98e679eea3cf57
Danny Gehl <dgehl@salesforce.com>
2025-02-26 20:40:58 +0100

    PDP image carousel

--- commerce-cloud-code/app_yourshophere/cartridge/partials/pdp/images.js
+++ commerce-cloud-code/app_yourshophere/cartridge/partials/pdp/images.js
@@ -3,12 +3,31 @@ exports.createModel = function createModel(options) {
 
     const model = {
         images: variationModel.getImages(options.settings.viewType).toArray()
-            .slice(0, options.settings.imageCount).map(image => ({
+            .slice(0, options.settings.imageCount).map((image, index) => ({
                 url: image.url,
                 alt: image.alt,
+                id: `slide-${index + 1}`, // Add unique ID for each slide
             })),
     };
     return model;
 };
 
-exports.template = model => model.images.map(image => `<img src="${image.url}" alt="${image.alt}" />`).join('\n');
+exports.template = model => `
+    <div class="image-slider">
+        <div class="slider-viewport">
+            <ul class="slider-track">
+                ${model.images.map(image => `
+                    <li class="slide" id="${image.id}">
+                        <input type="radio" name="slider" id="${image.id}-radio" ${image.id === 'slide-1' ? 'checked' : ''}>
+                        <img src="${image.url}" alt="${image.alt}" />
+                    </li>
+                `).join('\n')}
+            </ul>
+        </div>
+        <div class="slider-nav">
+            ${model.images.map(image => `
+                <label for="${image.id}-radio" class="nav-dot ${image.id === 'slide-1' ? 'active' : ''}"></label>
+            `).join('\n')}
+        </div>
+    </div>
+`;
