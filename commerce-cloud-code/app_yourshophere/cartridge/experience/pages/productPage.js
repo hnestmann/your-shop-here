2baf4401df86c3f94f062386fdf797659359aff1
Danny Gehl <dgehl@salesforce.com>
2025-02-27 14:57:51 +0100

    Implemented breadcrumbs, moved them to right column, fixed some issues in the PDP page

--- commerce-cloud-code/app_yourshophere/cartridge/experience/pages/productPage.js
+++ commerce-cloud-code/app_yourshophere/cartridge/experience/pages/productPage.js
@@ -1,8 +1,7 @@
-'use strict';
-var Template = require('dw/util/Template');
-var HashMap = require('dw/util/HashMap');
-var PageRenderHelper = require('*/cartridge/experience/utilities/PageRenderHelper.js');
-var RegionModelRegistry = require('*/cartridge/experience/utilities/RegionModelRegistry.js');
+const Template = require('dw/util/Template');
+const HashMap = require('dw/util/HashMap');
+const PageRenderHelper = require('*/cartridge/experience/utilities/PageRenderHelper.js');
+const RegionModelRegistry = require('*/cartridge/experience/utilities/RegionModelRegistry.js');
 
 /**
  * Render logic for the storepage.
@@ -32,28 +31,30 @@ function renderComponent (context) {
     }
 
     // automatically register configured regions
-    var metaDefinition = require('*/cartridge/experience/pages/productPage.json');
+    const metaDefinition = require('*/cartridge/experience/pages/productPage.json');
+
     model.regions = new RegionModelRegistry(page, metaDefinition);
 
     // Determine seo meta data.
     // Used in htmlHead.isml via common/layout/page.isml decorator.
-    model.CurrentPageMetaData = PageRenderHelper.getPageMetaData(page);
-    model.CurrentPageMetaData = {};
-    model.CurrentPageMetaData.title = page.pageTitle;
-    model.CurrentPageMetaData.description = page.pageDescription;
-    model.CurrentPageMetaData.keywords = page.pageKeywords;
+    const pageMetaData = require('*/cartridge/middleware/pageMetaData');
+
+    model.CurrentPageMetaData = request.pageMetaData;
+
+    // does it make sense to ignore page meta data?
+    pageMetaData.setPageMetaData(model.CurrentPageMetaData, model.product);
+    pageMetaData.setPageMetaTags(model.CurrentPageMetaData, model.product);
 
     if (PageRenderHelper.isInEditMode()) {
-        var HookManager = require('dw/system/HookMgr');
-        HookManager.callHook('app.experience.editmode', 'editmode');
+        require('dw/system/HookMgr').callHook('app.experience.editmode', 'editmode');
         model.resetEditPDMode = true;
     }
-    
+
     model.httpParameter = {};
 
     if (context.renderParameters) {
-        var queryString = JSON.parse(context.renderParameters).queryString; 
-        model.httpParameter = JSON.parse('{"' + queryString.replace(/&/g, '","').replace(/=/g,'":"') + '"}', function(key, value) { return key===""?value:decodeURIComponent(value) })
+        const queryString = JSON.parse(context.renderParameters).queryString;
+        model.httpParameter = JSON.parse(`{"${queryString.replace(/&/g, '","').replace(/=/g, '":"')}"}`, (key, value) => (key === '' ? value : decodeURIComponent(value)));
     }
     request.custom.model = model;
     // render the page
